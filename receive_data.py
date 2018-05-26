c3fae45ce1731ca55ec6780fc4fca7827ae29db7
birneamstiel <luckifri@yahoo.de>
2018-05-26 23:42:46 +0200

    Add decoding of serialized values.

--- receive_data.py
+++ receive_data.py
@@ -1,5 +1,6 @@
 import bluetooth
 import serial
+import json
 
 
 def connect():
@@ -22,13 +23,27 @@ data = ""
 while(True):
     try:
         data += socket.recv(1024)
-	data_end = data.find('\n')
-	if data_end != -1:
-		rec = data[:data_end]
-		print (data)
-		data = data[data_end+1:]
+        data_end = data.find('\n')
+        if data_end != -1:
+            rec = data[:data_end]
+            handle_value(data)
+            data = data[data_end+1:]
     except bluetooth.btcommon.BluetoothError as error:
         print "Caught BluetoothError: ", error
         time.sleep(5)
         socket = connect()
         pass
+
+def handle_value(value):
+    print plant_value_as_json(value)
+    return
+
+def plant_value_as_json(plantValue):
+    object = {}
+    sensorValues = value.split("#")
+    for sensorValue in sensorValues:
+        splittedSensorValue = sensorValue.split("|")
+        object[splittedSensorValue[0]] = sensorValue[splittedSensorValue[1]]
+    return json.dumps(data)
+
+
